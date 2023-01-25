1020b7c7176d33011157f33cd256a611d78253e7
Salih CELIKBAS <salih@salihcelikbas.com>
2023-01-25 10:14:45 +0100

    first commit

---
+++ src/main/java/fr/orsys/projet/plage/PlageApplication.java
@@ -0,0 +1,13 @@
+package fr.orsys.projet.plage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlageApplication.class, args);
+	}
+
+}
