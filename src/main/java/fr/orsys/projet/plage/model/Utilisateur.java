cfa1e95efcd989af7a12a757bb87eca6c964307e
Amine Drissaoui <Amine.drissaoui@gmail.com>
2023-01-25 11:38:37 +0100

    class Utilisateur

---
+++ src/main/java/fr/orsys/projet/plage/model/Utilisateur.java
@@ -0,0 +1,32 @@
+package fr.orsys.projet.plage.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "utilisateurs")
+public class Utilisateur {
+  
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+  
+    @Column(name = "nom")
+    private String nom;
+  
+    @Column(name = "prenom")
+    private String prenom;
+  
+    @Column(name = "email")
+    private String email;
+  
+    @Column(name = "mot_de_passe")
+    private String motDePasse;
+}
